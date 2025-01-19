80112e5e5a6ee98767abc74f9c048d5da202798d
jcrose1997 <jcrose1997@gmail.com>
2025-01-18 19:43:27 -0600

    Texture method started for skinning minigame

--- Assets/Scripts/Gameplay/Skinning.cs
+++ Assets/Scripts/Gameplay/Skinning.cs
@@ -6,18 +6,43 @@ public class Skinning : MonoBehaviour
 {
     public GameObject skinning;
     public GameObject bunny;
-    public Texture bunnyStart, bunnyEnd;
+    public Texture2D bunnyStart, bunnyEnd;
+    Texture2D textureArea;
+    Material mat;
+    Sprite sprite;
+
+    private void Start()
+    {
+        Vector2 origin = bunny.transform.position;
+        Vector2 size = new Vector2(bunnyStart.width, bunnyStart.height);
+
+
+        mat = new Material(Shader.Find("UI/Default"));
+        //textureArea = Instantiate(bunnyStart);
+        mat.mainTexture = bunnyStart;
+        //sprite = Sprite.Create(textureArea, new Rect(origin.x, origin.y, size.x, size.y), bunny.GetComponent<RectTransform>().pivot);
+        bunny.GetComponent<MeshRenderer>().material = mat;
+        
+    }
 
     public IEnumerator SkinningGameplay()
     {
-        Texture2D textureArea = new Texture2D((int)bunny.GetComponent<SpriteRenderer>().size.x, (int)bunny.GetComponent<SpriteRenderer>().size.y);
         skinning.SetActive(true);
+
+        Vector2 pos;
+        int posX, posY;
 
         while (!GameplayController.instance.isComplete)
         {
             if (Input.GetMouseButton(0))
             {
-                textureArea.SetPixel((int)Camera.main.ViewportToScreenPoint(Input.mousePosition).x, (int)Camera.main.ViewportToScreenPoint(Input.mousePosition).y, new Color(0, 1, 0));
+
+
+                //RectTransformUtility.ScreenPointToLocalPointInRectangle(sprite.rect, Input.mousePosition, Camera.main, out pos);
+                //posX = (int)pos.x;
+                //posY = (int)pos.y;
+                //Debug.Log(pos);
+                //textureArea.SetPixel(posX, posY, bunnyEnd.GetPixel(posX, posY));
             }
             yield return null;
         }
