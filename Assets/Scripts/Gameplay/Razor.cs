254e6d3c71800be9c74ae283cb79c9f5b0656686
jcrose1997 <jcrose1997@gmail.com>
2025-02-18 19:46:10 -0600

    FINALLY got skinning working

--- Assets/Scripts/Gameplay/Razor.cs
+++ Assets/Scripts/Gameplay/Razor.cs
@@ -1,12 +1,17 @@
+using System;
+using DG.Tweening;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Razor : MonoBehaviour
 {
-    float speed = 5.0f;
+    float speed = 7.0f;
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, GameplayController.instance.player.transform.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, -3), new Vector3(GameplayController.instance.player.transform.position.x, GameplayController.instance.player.transform.position.y, -3), speed * Time.deltaTime);
     }
 
 
--- Assets/Scripts/Gameplay/Skinning.cs
+++ Assets/Scripts/Gameplay/Skinning.cs
@@ -1,56 +1,72 @@
 using System.Collections;
+using Fungus;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Skinning : MonoBehaviour
 {
     public GameObject skinning;
-    public GameObject bunny;
-    public Texture2D bunnyStart, bunnyEnd;
-    Texture2D textureArea;
-    Material mat;
-    Sprite sprite;
+    public Camera cam;
+    public SpriteMask mask;
+    public GameObject razor, razorPrefab, linePrefab;
+    public GameObject skinObj;
+    public Texture2D skinStart, skinEnd;
 
     private void Start()
     {
-        Vector2 origin = bunny.transform.position;
-        Vector2 size = new Vector2(bunnyStart.width, bunnyStart.height);
+        razor = Instantiate(razorPrefab, new Vector3(0,0,-3), new Quaternion(0,0,0,0));
 
-
-        mat = new Material(Shader.Find("UI/Default"));
-        //textureArea = Instantiate(bunnyStart);
-        mat.mainTexture = bunnyStart;
-        //sprite = Sprite.Create(textureArea, new Rect(origin.x, origin.y, size.x, size.y), bunny.GetComponent<RectTransform>().pivot);
-        bunny.GetComponent<MeshRenderer>().material = mat;
-        
     }
 
     public IEnumerator SkinningGameplay()
     {
         skinning.SetActive(true);
-
-        Vector2 pos;
-        int posX, posY;
+        GameObject lineObj = Instantiate(linePrefab, new Vector3(0,0,0), new Quaternion(0,0,0,0));
+        LineRenderer line = lineObj.GetComponent<LineRenderer>();
+        line.positionCount = 0;
 
         while (!GameplayController.instance.isComplete)
         {
-            if (Input.GetMouseButton(0))
-            {
+            Vector3 position = razor.transform.position;
+            position.z = 0;
+            line.positionCount++;
+            line.SetPosition(line.positionCount-1, position);
+            AssignMask();
 
-
-                //RectTransformUtility.ScreenPointToLocalPointInRectangle(sprite.rect, Input.mousePosition, Camera.main, out pos);
-                //posX = (int)pos.x;
-                //posY = (int)pos.y;
-                //Debug.Log(pos);
-                //textureArea.SetPixel(posX, posY, bunnyEnd.GetPixel(posX, posY));
-            }
             yield return null;
         }
 
-        //bunny.GetComponent<SpriteRenderer>().sprite = bunnyEnd;
         skinning.SetActive(false);
 
         GameplayController.instance.GameActive();
     }
 
+    void AssignMask()
+    {
+        int height = Screen.height;
+        int width = Screen.width;
+        int depth = 10;
+
+        RenderTexture render = new RenderTexture(width, height, depth);
+        Rect rect = new Rect(0, 0, width, height);
+        Texture2D texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+
+        cam.targetTexture = render;
+        cam.Render();
+
+        RenderTexture currentRender = RenderTexture.active;
+        RenderTexture.active = render;
+        texture.ReadPixels(rect, 0,0);
+        texture.Apply();
+
+        cam.targetTexture = null;
+        RenderTexture.active = currentRender;
+        Destroy(render);
+
+        Sprite sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f), height/20);
+        sprite.name = "line";
+        mask.sprite = sprite;
+        
+    }
 }
+
